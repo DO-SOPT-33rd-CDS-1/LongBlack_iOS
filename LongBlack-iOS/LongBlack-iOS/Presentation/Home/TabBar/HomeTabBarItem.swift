6d0bff5dd251054aef337d817e374bf24c4f7dd4
정채은 <cce523@hanmail.net>
2023-11-19 00:18:59 +0900

    Feat [#3] TabBarItem 설정

--- LongBlack-iOS/LongBlack-iOS/Presentation/Home/TabBar/HomeTabBarItem.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/Home/TabBar/HomeTabBarItem.swift
@@ -5,4 +5,53 @@
 //  Created by 정채은 on 11/19/23.
 //
 
-import Foundation
+
+import UIKit
+
+enum TabBarItem: Int, CaseIterable {
+    case note
+    case alarm
+    case myPage
+}
+
+extension TabBarItem {
+    var title: String {
+        switch self {
+        case .note:   return StringLiterals.Home.TabBar.note
+        case .alarm:         return StringLiterals.Home.TabBar.alarm
+        case .myPage:         return StringLiterals.Home.TabBar.myPage
+        }
+    }
+}
+
+extension TabBarItem {
+    var Icon: UIImage? {
+        switch self {
+        case .note:  return ImageLiterals.TabBar.icNote
+        case .alarm:        return ImageLiterals.TabBar.icAlarm
+        case .myPage:        return ImageLiterals.TabBar.icMyPage
+        }
+    }
+    
+    var selectedIcon: UIImage? {
+        switch self {
+        case .note:  return ImageLiterals.TabBar.icNoteSelected.withRenderingMode(.alwaysOriginal)
+        case .alarm:        return ImageLiterals.TabBar.icAlarmSelected.withRenderingMode(.alwaysOriginal)
+        case .myPage:        return ImageLiterals.TabBar.icMyPageSelected.withRenderingMode(.alwaysOriginal)
+        }
+    }
+}
+
+extension TabBarItem {
+    public func asTabBarItem() -> UITabBarItem {
+        let tabBarItem = UITabBarItem(
+            title: title,
+            image: Icon,
+            selectedImage: selectedIcon
+        )
+        
+        tabBarItem.imageInsets = UIEdgeInsets(top: -5, left: 0, bottom: 0, right: 0)
+        
+        return tabBarItem
+    }
+}
