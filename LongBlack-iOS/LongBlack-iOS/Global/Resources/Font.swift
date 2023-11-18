7501f331a9b058bc581dbdb1b26474d0039b20fd
정채은 <cce523@hanmail.net>
2023-11-19 00:01:24 +0900

    Fix [#2] 폰트 이름 변경 사항 반영

--- LongBlack-iOS/LongBlack-iOS/Global/Resources/Font.swift
+++ LongBlack-iOS/LongBlack-iOS/Global/Resources/Font.swift
@@ -9,63 +9,63 @@ import UIKit
 
 extension UIFont {
 
-  class var headH134Light: UIFont {
+  class var h1Light: UIFont {
     return UIFont(name: "Pretendard-Light", size: 34.0)!
   }
 
-  class var headH228Bold: UIFont {
+  class var h2Bold: UIFont {
     return UIFont(name: "Pretendard-Bold", size: 28.0)!
   }
 
-  class var headH324Semibold: UIFont {
+  class var h3Semibold: UIFont {
     return UIFont(name: "Pretendard-SemiBold", size: 24.0)!
   }
 
-  class var headH422Semibold: UIFont {
+  class var h4Semibold: UIFont {
     return UIFont(name: "Pretendard-SemiBold", size: 22.0)!
   }
 
-  class var headH520Semibold: UIFont {
+  class var h5Semibold: UIFont {
     return UIFont(name: "Pretendard-SemiBold", size: 20.0)!
   }
 
-  class var headH618Bold: UIFont {
+  class var h6Bold: UIFont {
     return UIFont(name: "Pretendard-Bold", size: 18.0)!
   }
 
-  class var headH717Semibold: UIFont {
+  class var h7Semibold: UIFont {
     return UIFont(name: "Pretendard-SemiBold", size: 17.0)!
   }
 
-  class var bodyB116Regular: UIFont {
+  class var b1Regular: UIFont {
     return UIFont(name: "Pretendard-Regular", size: 16.0)!
   }
 
-  class var bodyB215Regular: UIFont {
+  class var b2Regular: UIFont {
     return UIFont(name: "Pretendard-SemiBold", size: 15.0)!
   }
 
-  class var bodyB314Medium: UIFont {
+  class var b3Medium: UIFont {
     return UIFont(name: "Pretendard-Medium", size: 14.0)!
   }
 
-  class var bodyB414Light: UIFont {
+  class var b4Light: UIFont {
     return UIFont(name: "Pretendard-Light", size: 14.0)!
   }
 
-  class var bodyB513Mudium: UIFont {
+  class var b5Mudium: UIFont {
     return UIFont(name: "Pretendard-SemiBold", size: 13.0)!
   }
 
-  class var bodyB613Regular: UIFont {
+  class var b6Regular: UIFont {
     return UIFont(name: "Pretendard-Regular", size: 13.0)!
   }
 
-  class var bodyB712Bold: UIFont {
+  class var b7Bold: UIFont {
     return UIFont(name: "Pretendard-Bold", size: 12.0)!
   }
 
-  class var bodyB812Light: UIFont {
+  class var b8Light: UIFont {
     return UIFont(name: "Pretendard-Light", size: 12.0)!
   }
 }
