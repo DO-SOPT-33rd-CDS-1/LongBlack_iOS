9ec0eb2c4ba1acb7817187af3c843a806e561885
정채은 <cce523@hanmail.net>
2023-11-18 13:15:15 +0900

    Chore [#1] 불필요한 코드 삭제

--- LongBlack-iOS/LongBlack-iOS/Presentation/ViewController.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/ViewController.swift
@@ -7,48 +7,8 @@
 
 import UIKit
 
-//import SnapKit
-//import Then
-
 class ViewController: BaseViewController {
     
-    let label = UILabel()
-    
-//    override func setStyle() {
-//        view.backgroundColor = .blue
-//    }
-//    
-//    override func setLayout() {
-//        
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setUI()
-//    }
-
-//
-//    private func setUI() {
-//        setStyle()
-//        setLayout()
-//    }
-//    
-//    private func setStyle() {
-//        view.backgroundColor = .white
-//        
-//        label.do {
-//            $0.text = StringLiterals.Home.Item.title
-//        }
-//        
-//    }
-//    
-//    private func setLayout() {
-//        view.addSubviews(label)
-//        
-//        label.snp.makeConstraints {
-//            $0.center.equalToSuperview()
-//        }
-//    }
 }
 
 
