091e75431be553d2a1e4d3196b0bfdbe4d4dea6d
bangMinji98 <whoru9806@gmail.com>
2023-11-21 13:21:01 +0900

    Feat [#5] filter button collectionView cell 구현

--- LongBlack-iOS/LongBlack-iOS/Presentation/Note/Cell/FilterButtonCollectionViewCell.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/Note/Cell/FilterButtonCollectionViewCell.swift
@@ -7,6 +7,45 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 class FilterButtonCollectionViewCell: UICollectionViewCell {
     
+    static let identifier: String = "FilterButtonCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        self.addSubviews(button, filter)
+        
+        filter.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(6)
+            $0.leading.trailing.equalToSuperview().inset(15)
+        }
+    }
+    
+    private var button = UIButton().then {
+        $0.backgroundColor = .clear
+        $0.frame = CGRect(x: 0, y: 0, width: 115, height: 28)
+        $0.layer.cornerRadius = 43
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor(red: 0.925, green: 0.925, blue: 0.925, alpha: 1).cgColor
+    }
+    
+    private var filter = UILabel().then {
+        $0.font = .b5Mudium
+        $0.textColor = .black
+    }
+    
+    func bindData(data: FilterButtonData) {
+        self.filter.text = data.filter
+    }
 }
