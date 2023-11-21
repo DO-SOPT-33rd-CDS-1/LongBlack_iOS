b51f5ed9bd736d6df4ed4648f4f29f938478a466
정채은 <cce523@hanmail.net>
2023-11-21 23:34:42 +1000

    Chore  [#3] 불필요한 코드 삭제

--- LongBlack-iOS/LongBlack-iOS/Global/Extensions/UITabBar+.swift
+++ LongBlack-iOS/LongBlack-iOS/Global/Extensions/UITabBar+.swift
@@ -12,6 +12,5 @@ extension UITabBar {
         UITabBar.appearance().shadowImage = UIImage()
         UITabBar.appearance().backgroundImage = UIImage()
         UITabBar.appearance().backgroundColor = .black
-//        UITabBar.appearance().alpha = 0.15
     }
 }
