d7ef102bd3793a5140bdd70c41aff17e19bd2e7d
bangMinji98 <whoru9806@gmail.com>
2023-11-21 13:21:47 +0900

    Feat [#5] filter button collectionView Cell horizontal Scroll 구현

--- LongBlack-iOS/LongBlack-iOS/Presentation/Note/Cell/FilterButtonMakeCollectionViewCell.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/Note/Cell/FilterButtonMakeCollectionViewCell.swift
@@ -7,6 +7,58 @@
 
 import UIKit
 
-class FilterButtonMakeCollectionViewCell: UICollectionViewCell {
+import Then
+import SnapKit
+
+class FilterButtonMakeCollectionViewCell: UICollectionViewCell, UICollectionViewDelegate {
     
+    static let identifier: String = "FilterButtonMakeCollectionViewCell"
+    
+    let collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+       layout.scrollDirection = .horizontal
+       let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+       collectionView.backgroundColor = .clear
+       collectionView.showsHorizontalScrollIndicator = false
+       return collectionView
+   }()
+    
+    private func setCollectionViewConfig() {
+        self.collectionView.register(FilterButtonCollectionViewCell.self, forCellWithReuseIdentifier: FilterButtonCollectionViewCell.identifier)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setLayout()
+        setCollectionViewConfig()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+}
+
+
+extension FilterButtonMakeCollectionViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 115, height: 28)
+        }
+}
+extension FilterButtonMakeCollectionViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filterButtonData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: FilterButtonCollectionViewCell.identifier, for: indexPath) as? FilterButtonCollectionViewCell else {return UICollectionViewCell()}
+        item.bindData(data: filterButtonData[indexPath.item])
+        return item
+    }
 }
