46e5b94af669b93d8c97c8425f1274ba1329989a
bangMinji98 <whoru9806@gmail.com>
2023-11-22 18:59:27 +0900

    Feat [#5] NoteData 이용하여 NoteViewCell 구현
    cell의 요소들 레이아웃 잡아줘야함

--- LongBlack-iOS/LongBlack-iOS/Presentation/Note/Cell/NoteCollectionViewCell.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/Note/Cell/NoteCollectionViewCell.swift
@@ -9,4 +9,61 @@ import UIKit
 
 class NoteCollectionViewCell: UICollectionViewCell {
     
+    static let identifier: String = "NoteCollectionViewCell"
+    
+    private var itemRow: Int?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    private var image: UIImage?
+    private var title = UILabel().then {
+        $0.font = .h5Semibold
+        $0.textColor = .black
+    }
+    
+    private var author = UILabel().then {
+        $0.font = .b3Medium
+        $0.textColor = .black
+    }
+    
+    private var divider = UILabel().then {
+        $0.textColor = .subGray3
+    }
+    
+    private var nickname = UILabel().then {
+        $0.font = .b3Medium
+        $0.textColor = .black
+    }
+    
+    private var state: UIImage?
+    
+    private var background = UIView().then {
+        $0.frame = CGRect(x: 0, y: 0, width: 335, height: 139)
+    }
+    
+    func bindData(data: NoteData) {
+        self.image = UIImage(named: data.image)
+        self.title.text = data.title
+        self.author.text = data.author
+        self.divider.text = data.divider
+        self.nickname.text = data.nickname
+        if data.state {
+                self.state = UIImage(named: "ic_arrow_left")
+            } else {
+                self.state = UIImage(named: "ic_search")
+            }
+        //아직 에셋 추가 안해서 임시이미지로 넣어줌
+        self.background.backgroundColor = UIColor(named: data.backgroundColor)
+    }
 }
--- LongBlack-iOS/LongBlack-iOS/Presentation/Note/data/NoteData.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/Note/data/NoteData.swift
@@ -15,10 +15,10 @@ struct NoteData {
     let divider: String
     let nickname: String
     let state: Bool
-    let backgroundColor: UIColor
+    let backgroundColor: String
     
     
-    init(image: String, title: String, author: String, divider: String, nickname: String, state: Bool, backgroundColor: UIColor) {
+    init(image: String, title: String, author: String, divider: String, nickname: String, state: Bool, backgroundColor: String) {
         self.image = image
         self.title = title
         self.author = author
@@ -30,12 +30,12 @@ struct NoteData {
 }
 
 var noteData: [NoteData] = [
-    .init(image: "image1", title: "오픈갤러리 : 내 거실이 갤러리, 미술 시장의 빈틈을 파고든 원화 구독 ···", author: "박희정", divider: "|", nickname: "P", state: true, backgroundColor: .mainMainOrange),
-    .init(image: "image2", title: "무스토이 : OMG와 슈퍼해피 정신으로, 아마존 1위 장난감이 되다", author: "방민지", divider: "|", nickname: "B", state: false, backgroundColor: .subcolorGreen),
-    .init(image: "image3", title: "애시드 리그 : 커피·고수·김치맛 식초, 4000개의 슈퍼마켓 매대를 꿰차다", author: "손명지", divider: "|", nickname: "S", state: false, backgroundColor: .subcolorGreen),
-    .init(image: "image4", title: "조병수 : 10년 뒤 서울엔 산과 강, 바람이 있을까” 건축가가 묻다", author: "신민철", divider: "|", nickname: "S", state: true, backgroundColor: .mainPointBlue),
-    .init(image: "image5", title: "코오롱스포츠 : 연 8000억 매출의 50년 아웃도어 브랜드, 오리진을 ···", author: "이연진", divider: "|", nickname: "L", state: true, backgroundColor: .mainPointBlue),
-    .init(image: "image6", title: "First, Learn to practice : 조금 더 계속하는 바로 그 순간", author: "이우제", divider: "|", nickname: "L", state: true, backgroundColor: .subcolorSand),
-    .init(image: "image7", title: "엄커피 : 아버지가 브라질에 뿌린 커피콩, 아들을 바리스타 챔피언으로 ···", author: "이준희", divider: "|", nickname: "L", state: false, backgroundColor: .mainPointBlue),
-    .init(image: "image8", title: "아루히 : 손님과의 케미스트리로, ‘스강신청’ 오마카세 집이 되다", author: "정채은", divider: "|", nickname: "L", state: false, backgroundColor: .mainPointBlue),
+    .init(image: "image1", title: "오픈갤러리 : 내 거실이 갤러리, 미술 시장의 빈틈을 파고든 원화 구독 ···", author: "박희정", divider: "|", nickname: "P", state: true, backgroundColor: "#FF5126"),
+    .init(image: "image2", title: "무스토이 : OMG와 슈퍼해피 정신으로, 아마존 1위 장난감이 되다", author: "방민지", divider: "|", nickname: "B", state: false, backgroundColor: "#A3F4A5"),
+    .init(image: "image3", title: "애시드 리그 : 커피·고수·김치맛 식초, 4000개의 슈퍼마켓 매대를 꿰차다", author: "손명지", divider: "|", nickname: "S", state: false, backgroundColor: "#A4F4A3"),
+    .init(image: "image4", title: "조병수 : 10년 뒤 서울엔 산과 강, 바람이 있을까” 건축가가 묻다", author: "신민철", divider: "|", nickname: "S", state: true, backgroundColor: "#5A61FF"),
+    .init(image: "image5", title: "코오롱스포츠 : 연 8000억 매출의 50년 아웃도어 브랜드, 오리진을 ···", author: "이연진", divider: "|", nickname: "L", state: true, backgroundColor: "#5962FF"),
+    .init(image: "image6", title: "First, Learn to practice : 조금 더 계속하는 바로 그 순간", author: "이우제", divider: "|", nickname: "L", state: true, backgroundColor: "#DEC197"),
+    .init(image: "image7", title: "엄커피 : 아버지가 브라질에 뿌린 커피콩, 아들을 바리스타 챔피언으로 ···", author: "이준희", divider: "|", nickname: "L", state: false, backgroundColor: "#FF5126"),
+    .init(image: "image8", title: "아루히 : 손님과의 케미스트리로, ‘스강신청’ 오마카세 집이 되다", author: "정채은", divider: "|", nickname: "L", state: false, backgroundColor: "#5962FF"),
     ]
