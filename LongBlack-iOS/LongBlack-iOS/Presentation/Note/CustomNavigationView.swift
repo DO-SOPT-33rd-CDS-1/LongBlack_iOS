a23c08934624ec442b74904d34a6b3a3303b14ff
bangMinji98 <whoru9806@gmail.com>
2023-11-23 06:27:08 +0900

    Add [#5] CustomNavigationView 파일 생성

--- LongBlack-iOS/LongBlack-iOS/Presentation/Note/CustomNavigationView.swift
+++ LongBlack-iOS/LongBlack-iOS/Presentation/Note/CustomNavigationView.swift
@@ -5,4 +5,61 @@
 //  Created by 방민지 on 11/23/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class CustomNavigationView: UIView {
+
+    private let backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("노트", for: .normal)
+        button.setImage(UIImage(named: "ic_arrow_left"), for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+        button.setTitleColor(UIColor.black, for: .normal)
+        return button
+    }()
+
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "검색어를 입력하세요"
+        searchBar.layer.cornerRadius = 32
+        return searchBar
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+        setupConstraints()
+    }
+
+    private func setupViews() {
+        addSubviews(backButton, searchBar)
+        
+        self.backgroundColor = .white
+    }
+
+    private func setupConstraints() {
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(72)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalToSuperview().offset(21)
+        }
+
+        searchBar.snp.makeConstraints {
+            $0.width.equalTo(210)
+//            $0.height.equalTo(36)
+            $0.leading.equalToSuperview().offset(145)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
