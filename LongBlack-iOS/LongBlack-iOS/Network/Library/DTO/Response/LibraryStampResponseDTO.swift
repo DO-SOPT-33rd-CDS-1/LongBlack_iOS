2059a2b88989d204a23edf94c0275cbf83495b97
chaentopia <cce523@hanmail.net>
2023-11-28 14:17:33 +0900

    Feat [#18] responseDTO 생성

---
+++ LongBlack-iOS/LongBlack-iOS/Network/Library/DTO/Response/LibraryStampResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  LibraryStampResponseDTO.swift
+//  LongBlack-iOS
+//
+//  Created by 정채은 on 11/28/23.
+//
+
+import Foundation
+
+struct LibraryStampResponseDTO: Codable {
+    let stampCount: Int
+}
+
---
+++ LongBlack-iOS/LongBlack-iOS/Network/Library/Service/LibraryService.swift
@@ -0,0 +1,8 @@
+//
+//  LibraryStampService.swift
+//  LongBlack-iOS
+//
+//  Created by 정채은 on 11/28/23.
+//
+
+import Foundation
